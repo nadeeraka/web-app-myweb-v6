b6980703d05d7f8af2cbb3d53615121d40fd7cf2
nadeeraka <global-nadeerakan@gmail.com>
2023-09-19 11:56:28 +0530

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
+import Nav from "../components/nav";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-  );
+  return <main className=""></main>;
 }
---
+++ src/components/nav/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <div className="z-[999] relative  flex justify-center">
+      <div
+        className="   fixed top-10   border-white border-opacity-40 
+       bg-white bg-opacity-80 shadow-lg shadow-black/[0.3] backdrop-blur-[0.5rem] 
+       lg:h-[3.5rem] lg:w-[60%] lg:rounded-full md:h-[3.25rem] md:w-[50%] md:rounded-full sm:w-full sm:h-[3rem] 
+       sm:rounded-none "
+      >
+        {/* <a href="">Home</a> */}
+      </div>
+    </div>
+  );
+}
