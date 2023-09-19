8dd68901d61f4b0b7d81a8351a26b940805a1474
nadeeraka <global-nadeerakan@gmail.com>
2023-09-19 12:17:58 +0530

    2

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,16 +1,24 @@
+"use client";
 import React from "react";
+import { links } from "@/lib/data";
+import { motion } from "framer-motion";
 
 export default function Nav() {
   return (
     <div className="z-[999] relative  flex justify-center">
-      <div
+      <motion.div
         className="   fixed top-10   border-white border-opacity-40 
        bg-white bg-opacity-80 shadow-lg shadow-black/[0.3] backdrop-blur-[0.5rem] 
        lg:h-[3.5rem] lg:w-[60%] lg:rounded-full md:h-[3.25rem] md:w-[50%] md:rounded-full sm:w-full sm:h-[3rem] 
        sm:rounded-none "
+        initial={{ y: -100, x: -100, opacity: 0 }}
+        animate={{ y: 0, x: 0, opacity: 1 }}
+        transition={{ ease: "easeOut", duration: 1 }}
       >
-        {/* <a href="">Home</a> */}
-      </div>
+        {links.map(({ name, hash }) => (
+          <a href={hash}>{name}</a>
+        ))}
+      </motion.div>
     </div>
   );
 }
