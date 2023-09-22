d9c17d5d53000d4b559826049f426e8a220dcebc
nadeeraka <global-nadeerakan@gmail.com>
2023-09-22 13:43:20 +0530

    🥸

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import Image from "next/image";
+import IMG from "../../../public/img1.jpg";
 
 export default function () {
   return (
-    <div>
-      <div className=""></div>
-    </div>
+    <section>
+      <div className="flex justify-center items-center w-full">
+        <div className=" ">
+          <Image
+            src={IMG}
+            alt=""
+            width={200}
+            height={50}
+            priority={true}
+            quality={100}
+            className="w-24 h-24 rounded-full border-white border-[3px] object-cover shadow-xl "
+          />
+          <span>👌</span>
+        </div>
+      </div>
+    </section>
   );
 }
